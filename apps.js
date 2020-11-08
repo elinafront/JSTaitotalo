3ae04f0589ce80af926ae58531e04f97decc6c45
elinafront <65065023+elinafront@users.noreply.github.com>
2020-11-08 11:55:25 +0200

    Fixed navbar

--- apps.js
+++ apps.js
@@ -21,7 +21,11 @@ function print() {
        // click on second button -> message
       function printName() {
         var etunimi = document.getElementById('firstName').value;
-        document.getElementById('hello').innerHTML = "Hei " + etunimi;
+        document.getElementById('hello').innerHTML = "Hei " + etunimi + "! Mitä kuuluu?";
+        if(etunimi == ""){
+          alert('Et antanut etunimeäsi');
+          return false;
+        }
       }
        // sum for two numbers
       function sum() {
@@ -33,9 +37,8 @@ function print() {
         // how many times
       function times() {
         var montakoKertaa = parseInt(document.getElementById('firstNum').value);
-        for(i = 0; i < montakoKertaa; i++) {
+        for(i = 0; i < 11; i++) {
           tulos = i*montakoKertaa;
           document.getElementById('kertomat').innerHTML += tulos + ' ';
-        }
       }
-  
+    }
